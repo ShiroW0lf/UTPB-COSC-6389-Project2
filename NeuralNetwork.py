2e551cdd61badd843d9fc8710d8b09d77b71ece3
Aswin <lohaniaswin@gmail.com>
2024-11-14 12:16:27 -0600

    first iteration

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,188 +1,283 @@
-import math
-import random
+import numpy as np
+import pandas as pd
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-num_inputs = 10
-num_hidden_layers = 3
-hidden_layer_width = 12
-num_outputs = 4
-neuron_scale = 5
-axon_scale = 4
-learning_rate = 0.1
+# Load the dataset
+from ucimlrepo import fetch_ucirepo
 
-training_data_size = 100
-
-
-def sigmoid(total):
-    e = math.exp(total)
-    return e / (1 + e)
+# fetch dataset
+banknote_authentication = fetch_ucirepo(id=267)
+X = banknote_authentication.data.features
+y = banknote_authentication.data.targets
 
 
+# Activation Functions
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_derivative(x):
+    return np.where(x > 0, 1, 0)
+
+
+def tanh(x):
+    return np.tanh(x)
+
+
+def tanh_derivative(x):
+    return 1 - np.tanh(x) ** 2
+
+
+# Cost Functions
+def mean_squared_error(y_true, y_pred):
+    return np.mean(np.square(y_true - y_pred))
+
+
+def binary_crossentropy(y_true, y_pred):
+    y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)  # To prevent log(0)
+    return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+
+# Neuron Class
 class Neuron:
-    def __init__(self, x, y, input_idx=-1, bias=0.0):
-        self.x = x
-        self.y = y
-        self.inputs = []
-        self.outputs = []
-        self.index = input_idx
-        self.bias = bias
-        self.result = 0.0
-        self.error = 0.0
+    def __init__(self, input_size, output_size, activation_fn, activation_deriv_fn):
+        self.weights = np.random.randn(input_size, output_size)
+        self.biases = np.random.randn(output_size)
+        self.activation_fn = activation_fn
+        self.activation_deriv_fn = activation_deriv_fn
 
-    def connect_input(self, in_n):
-        in_axon = Axon(in_n, self)
-        self.inputs.append(in_axon)
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = self.activation_fn(np.dot(self.inputs, self.weights) + self.biases)
+        return self.output
 
-    def connect_output(self, out_n):
-        out_axon = Axon(self, out_n)
-        self.outputs.append(out_axon)
+    def backward(self, d_output):
+        d_activation = d_output * self.activation_deriv_fn(self.output)
+        self.d_weights = np.dot(self.inputs.T, d_activation)
+        self.d_biases = np.sum(d_activation, axis=0)
+        d_input = np.dot(d_activation, self.weights.T)
+        return d_input
 
-    def forward_prop(self, inputs):
-        if self.result != 0.0:
-            return self.result
-        total = 0.0
-        if self.index >= 0:
-            total = inputs[self.index]
-            #print(f'Input neuron {self.index} found value {inputs[self.index]}')
-        else:
-            for in_axon in self.inputs:
-                in_n = in_axon.input
-                in_n.forward_prop(inputs)
-                in_val = in_n.result * in_axon.weight
-                #print(f'Adding weighted value {in_val} from input')
-                total += in_val
-            #print(f'Neuron computed sum {total} from inputs')
-        total += self.bias
-        #print(f'Biased total is {total}')
-        self.result = sigmoid(total)
-        #print(f'Final neuron output is {self.result}')
-        #print()
-
-    def back_prop(self):
-        if self.error != 0.0:
-            return
-        for out_axon in self.outputs:
-            out_n = out_axon.output
-            out_n.back_prop()
-        gradient = self.result * (1.0 - self.result)
-        delta = self.error * gradient
-        # print(f'Error calc: {self.error} {gradient} {delta}')
-        if self.index == -1:
-            for in_axon in self.inputs:
-                in_n = in_axon.input
-                in_n.error += in_n.error * in_axon.weight
-                in_axon.weight -= delta * in_n.result * learning_rate
-        self.bias -= delta * learning_rate
-
-    def draw(self, canvas, color='black'):
-        canvas.create_oval(self.x - neuron_scale, self.y - neuron_scale, self.x + neuron_scale, self.y + neuron_scale, fill=color)
+    def update_weights(self, learning_rate):
+        self.weights -= learning_rate * self.d_weights
+        self.biases -= learning_rate * self.d_biases
 
 
-class Axon:
-    def __init__(self, in_n, out_n, weight=0.0):
-        self.input = in_n
-        self.output = out_n
-        self.weight = weight
-
-    def draw(self, canvas, color='grey'):
-        canvas.create_line(self.input.x,
-                           self.input.y,
-                           self.output.x,
-                           self.output.y,
-                           fill=color,
-                           width=axon_scale)
-
-
+# Network Class
 class Network:
-    def __init__(self):
-        self.inputs = []
-        self.hidden_layers = []
-        self.outputs = []
-        for idx in range(num_inputs):
-            in_n = Neuron(0, 0, idx, 1.0)
-            self.inputs.append(in_n)
-        for layer in range(num_hidden_layers):
-            self.hidden_layers.append([])
-            for _ in range(hidden_layer_width):
-                hidden_n = Neuron(0, 0)
-                self.hidden_layers[layer].append(hidden_n)
-                if layer == 0:
-                    for in_n in self.inputs:
-                        hidden_n.connect_input(in_n)
-                        in_n.connect_output(hidden_n)
-                else:
-                    for h_n in self.hidden_layers[layer-1]:
-                        hidden_n.connect_input(h_n)
-                        h_n.connect_output(hidden_n)
-        for _ in range(num_outputs):
-            out_n = Neuron(0, 0)
-            self.outputs.append(out_n)
-            for h_n in self.hidden_layers[num_hidden_layers-1]:
-                out_n.connect_input(h_n)
-                h_n.connect_output(out_n)
+    def __init__(self, input_size, hidden_layer_width, num_outputs, activation_fn, activation_deriv_fn, cost_fn):
+        self.hidden_layer = Neuron(input_size, hidden_layer_width, activation_fn, activation_deriv_fn)
+        self.output_layer = Neuron(hidden_layer_width, num_outputs, sigmoid, sigmoid_derivative)
+        self.cost_fn = cost_fn
 
-    # Used in both train() and test() to get output from the current network
-    def forward_prop(self, inputs):
-        print(f'Performing forward propagation')
-        for in_n in self.inputs:
-            in_n.result = 0.0
-        for h_layer in self.hidden_layers:
-            for h_n in h_layer:
-                h_n.result = 0.0
-        for out_n in self.outputs:
-            out_n.result = 0.0
-            out_n.forward_prop(inputs)
+    def forward(self, X):
+        hidden_output = self.hidden_layer.forward(X)
+        output = self.output_layer.forward(hidden_output)
+        return output
 
-    # Used with train() to modify weights and biases to reduce error
-    def back_prop(self):
-        print(f'Performing backward propagation')
-        for h_layer in self.hidden_layers:
-            for h_n in h_layer:
-                h_n.error = 0.0
-        for out_n in self.outputs:
-            out_n.error = 0.0
-        for in_n in self.inputs:
-            in_n.error = 0.0
-            in_n.back_prop()
+    def backward(self, X, y, output):
+        cost_error = output - y
+        d_output = cost_error
+        d_hidden = self.output_layer.backward(d_output)
+        self.hidden_layer.backward(d_hidden)
 
-    # Given a datum, produce an output and compare against the target, then perform backprop to reduce error
-    # returns: nothing
-    def train(self, data):
-        self.forward_prop(data.inputs)
-        for x in range(num_outputs):
-            self.outputs[x].error = data.outputs[x] - self.outputs[x].result
-            print(f'Output {x} error is {self.outputs[x].error}')
-        self.back_prop()
+    def update(self, learning_rate):
+        self.hidden_layer.update_weights(learning_rate)
+        self.output_layer.update_weights(learning_rate)
 
-    # Given a datum, produce an output
-    # returns: the network output/selection/prediction
-    def test(self, data):
-        self.forward_prop(data.inputs)
-        out_max = 0.0
-        for out_n in self.outputs:
-            out_max = max(out_max, out_n.result)
-        return out_max
+    def train(self, X, y, num_epochs, learning_rate, progress_callback):
+        for epoch in range(num_epochs):
+            for i in range(len(X)):
+                output = self.forward(X[i].reshape(1, -1))
+                self.backward(X[i].reshape(1, -1), y[i], output)
+                self.update(learning_rate)
+
+            if epoch % 100 == 0:
+                predictions = self.test_network(X)
+                cost = self.cost_fn(y, predictions)
+                progress_callback(epoch, cost)
+
+    def test_network(self, X):
+        predictions = []
+        for i in range(len(X)):
+            output = self.forward(X[i].reshape(1, -1))
+            predictions.append(output)
+        return np.array(predictions)
 
 
-class RandData:
-    def __init__(self):
-        self.inputs = []
-        self.outputs = []
+# Function to select activation function and cost function
+def get_selected_functions(activation_var, cost_var):
+    activation_choice = activation_var.get()
+    if activation_choice == 'Sigmoid':
+        activation_fn = sigmoid
+        activation_deriv_fn = sigmoid_derivative
+    elif activation_choice == 'ReLU':
+        activation_fn = relu
+        activation_deriv_fn = relu_derivative
+    elif activation_choice == 'Tanh':
+        activation_fn = tanh
+        activation_deriv_fn = tanh_derivative
 
-    def generate(self):
-        for _ in range(num_inputs):
-            self.inputs.append(random.random()*10.0-5.0)
-        for _ in range(num_outputs):
-            self.outputs.append(random.random())
+    cost_choice = cost_var.get()
+    if cost_choice == 'Mean Squared Error':
+        cost_fn = mean_squared_error
+    elif cost_choice == 'Binary Cross-Entropy':
+        cost_fn = binary_crossentropy
+
+    return activation_fn, activation_deriv_fn, cost_fn
 
 
-def train():
-    global training_data_size
-    network = Network()
-    for _ in range(training_data_size):
-        dat = RandData()
-        dat.generate()
-        network.train(dat)
+
+# Function to update the progress label, plot loss, and visualize the neural network
+
+def update_progress(epoch, cost, ax, canvas, costs, progress_label, network, ax_nn, canvas_nn):
+    progress_label.config(text=f"Epoch {epoch} - Cost: {cost:.4f}")
+    progress_label.update()
+
+    # Plot the neural network loss (cost) over time
+    costs.append(cost)
+    ax.clear()
+    ax.set_title("Neural Network Loss")
+    ax.plot(costs, label="Cost", color="blue")
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Cost")
+    ax.legend()
+
+    # Visualize the neural network architecture (can be updated each epoch if needed)
+    ax_nn.clear()
+    ax_nn.set_title("Neural Network Architecture")
+
+    # Visualizing input and output layer
+    ax_nn.scatter([0] * len(network.hidden_layer.weights), range(len(network.hidden_layer.weights)), s=500,
+                  color='lightblue', label="Input Layer")
+    ax_nn.scatter([1] * len(network.output_layer.weights), range(len(network.output_layer.weights)), s=500,
+                  color='orange', label="Output Layer")
+
+    # Drawing connections between layers
+    for i in range(len(network.hidden_layer.weights)):
+        for j in range(len(network.output_layer.weights)):
+            ax_nn.plot([0, 1], [i, j], color='gray')
+
+    ax_nn.legend()
+    canvas_nn.draw()
+
+    # Redraw the loss graph
+    canvas.draw()
 
 
-if __name__ == '__main__':
-    train()
+# Updated start_training function with the corrected update_progress call
+def start_training(activation_var, cost_var, epochs_var, learning_rate_var, progress_label, training_label, canvas, ax,
+                   canvas_nn, ax_nn):
+    activation_fn, activation_deriv_fn, cost_fn = get_selected_functions(activation_var, cost_var)
+
+    # Initialize the network
+    input_size = X.shape[1]
+    hidden_layer_width = 64
+    num_outputs = 1
+    learning_rate = float(learning_rate_var.get())
+    num_epochs = int(epochs_var.get())
+
+    network = Network(input_size, hidden_layer_width, num_outputs, activation_fn, activation_deriv_fn, cost_fn)
+
+    # List to store cost history for plotting
+    costs = []
+
+    # Function to update the progress label during training
+    def progress_callback(epoch, cost):
+        update_progress(epoch, cost, ax, canvas, costs, progress_label, network, ax_nn, canvas_nn)
+
+    # Update the training label
+    training_label.config(text="Training...")
+    training_label.update()
+
+    # Train the network
+    network.train(X.values, y.values, num_epochs, learning_rate, progress_callback)
+
+    # After training, reset training label
+    training_label.config(text="Training Completed!")
+    training_label.update()
+
+
+# Create the GUI
+
+def create_ui():
+    root = tk.Tk()
+    root.title("Neural Network Trainer")
+
+    # Activation Function Selection
+    activation_label = tk.Label(root, text="Select Activation Function:")
+    activation_label.grid(row=0, column=0)
+    activation_var = ttk.Combobox(root, values=["Sigmoid", "ReLU", "Tanh"])
+    activation_var.set("Sigmoid")
+    activation_var.grid(row=0, column=1)
+
+    # Cost Function Selection
+    cost_label = tk.Label(root, text="Select Cost Function:")
+    cost_label.grid(row=1, column=0)
+    cost_var = ttk.Combobox(root, values=["Mean Squared Error", "Binary Cross-Entropy"])
+    cost_var.set("Mean Squared Error")
+    cost_var.grid(row=1, column=1)
+
+    # Number of Epochs Input
+    epochs_label = tk.Label(root, text="Number of Epochs:")
+    epochs_label.grid(row=2, column=0)
+    epochs_var = tk.Entry(root)
+    epochs_var.insert(0, "1000")
+    epochs_var.grid(row=2, column=1)
+
+    # Learning Rate Input
+    learning_rate_label = tk.Label(root, text="Learning Rate:")
+    learning_rate_label.grid(row=3, column=0)
+    learning_rate_var = tk.Entry(root)
+    learning_rate_var.insert(0, "0.01")
+    learning_rate_var.grid(row=3, column=1)
+
+    # Progress Label
+    progress_label = tk.Label(root, text="Training Progress")
+    progress_label.grid(row=4, column=0, columnspan=2)
+
+    # Training Label
+    training_label = tk.Label(root, text="Ready to Train")
+    training_label.grid(row=5, column=0, columnspan=2)
+
+    # Create the plot for loss visualization
+    fig, ax = plt.subplots(figsize=(6, 4))
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.get_tk_widget().grid(row=7, column=0, columnspan=2)
+    ax.axis('off')  # Hide axes
+
+    # Create the plot for neural network architecture visualization
+    fig_nn, ax_nn = plt.subplots(figsize=(6, 4))
+    canvas_nn = FigureCanvasTkAgg(fig_nn, master=root)
+    canvas_nn.get_tk_widget().grid(row=8, column=0, columnspan=2)
+    ax_nn.axis('off')  # Hide axes
+
+    # Train Button
+    train_button = tk.Button(root, text="Start Training",
+                             command=lambda: start_training(
+                                 activation_var, cost_var, epochs_var, learning_rate_var,
+                                 progress_label, training_label, canvas, ax, canvas_nn, ax_nn))
+    train_button.grid(row=6, column=0, columnspan=2)
+
+    # Start the GUI loop
+    root.mainloop()
+
+
+# Main function to run the UI
+def main():
+    create_ui()
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,25 +1,162 @@
-# UTPB-COSC-6389-Project2
-This repo contains the assignment and provided code base for Project 2 of the graduate Computational Biomimicry class.
+# Neural Network Trainer
 
-The goals of this project are:
-1) Learn the basics of how neural networks work, and how to build and use them.
-2) Understand the kinds of problems that neural networks are best at solving, and how to apply them to find solutions.
+This project implements a basic neural network from scratch using Python and Tkinter for the graphical user interface (GUI). The network supports binary and real-valued inputs and offers a variety of activation functions (Sigmoid, ReLU, Tanh) and cost functions (Mean Squared Error, Binary Cross-Entropy). The model can be trained on a dataset (e.g., the Banknote Authentication dataset from UCI) and visualizes the training progress in real-time.
 
-Description:
-The provided Python code base includes an implementation of a basic neural network.  Right now, it uses completely random input values and attempts to optimize or "learn" to produce a given output from those inputs.
+## Features
+- **Custom Neural Network**: Built from scratch using Python (without any specialized libraries like TensorFlow or PyTorch).
+- **Activation Functions**: Supports Sigmoid, ReLU, and Tanh activation functions.
+- **Cost Functions**: Includes Mean Squared Error and Binary Cross-Entropy cost functions.
+- **Real-Time Training Visualization**: Real-time visualization of the neural network's loss progress during training.
+- **GUI Interface**: Built using Tkinter, allowing users to easily interact with the neural network by configuring training parameters and visualizing the network architecture and loss graph.
+- **Interactive Plots**: Graphical representations of the neural network architecture and the training loss graph, providing intuitive feedback during the training process.
 
-Your task is twofold: first, you're going to find a problem that you can solve using a neural network that only handles binary or real-valued inputs and makes some sort of choice based on those inputs; second, you're going to implement additional activation/cost functions and provide means within the UI of configuring the network to use the function desired by the user during runtime.
+## Requirements
 
-This site (https://www.kaggle.com/datasets) seems to be a database of datasets that you will likely find useful for both projects 2 and 3.
+Make sure you have Python 3.x installed on your system. You'll also need the following Python packages:
 
-As with Project 1, your application must generate the neural networks, display them on a canvas, and update them in real time as the weight values change.
+- `numpy`
+- `pandas`
+- `tkinter`
+- `matplotlib`
+- `ucimlrepo` (for dataset fetching)
 
-You are not allowed to make use of any libraries related to neural networks in the code for this project.  The implementation of the network construction, operation, forward and backward propagation, training, and testing must all be your own.
+You can install the required packages by running:
 
-Grading criteria:
-1) If the code submitted via your pull request does not compile, the grade is zero.
-2) If the code crashes due to forseeable unhandled exceptions, the grade is zero.
-3) For full points, the code must correctly perform the relevant algorithms and display the network in real time, via the UI.
+```bash
+pip install numpy pandas matplotlib ucimlrepo
 
-Deliverables:
-A Python application which provides a correct implementation of a neural network generation and training system, and allows the user to define whether the hidden neurons use the sigmoid, tanh, or RelU function for their activation.  The selection of activation function for the output neurons will depend upon the choice of problem, but should be matched to the problem correctly.
+
+### Key Sections:
+1. **Features**: Overview of the core functionality of the project.
+2. **Requirements**: Information about necessary libraries and installation steps.
+3. **Installation**: Instructions on how to set up the project locally.
+4. **Usage**: A guide on how to use the GUI and train the neural network.
+5. **Neural Network Architecture**: Describes the model's structure and functionality.
+6. **Example Dataset**: Description of the dataset used in the project.
+7. **Future Enhancements**: Ideas for improving the project.
+8. **License**: Licensing information (you can change this based on the license you choose).
+9. **Acknowledgements**: Credit to third-party resources used in the project.
+
+# Neural Network Trainer
+
+## Usage
+
+### Step 1: Choose Activation and Cost Functions
+
+In the GUI, you can choose:
+
+- **Activation Function**: Sigmoid, ReLU, or Tanh.
+- **Cost Function**: Mean Squared Error or Binary Cross-Entropy.
+
+### Step 2: Set Hyperparameters
+
+Set the following hyperparameters:
+
+- **Number of Epochs**: The number of training iterations.
+- **Learning Rate**: The rate at which the model adjusts its weights during training.
+
+### Step 3: Start Training
+
+Click the **Start Training** button to begin the training process. The neural network will train and update in real-time, showing the current training progress (epoch and cost) and a graphical representation of the loss over epochs.
+
+### Step 4: Monitor Training Progress
+
+The following will be displayed during training:
+
+- **Epoch**: The current epoch number.
+- **Cost**: The current value of the loss function.
+- **Loss Graph**: A plot of the loss during training, showing how the model is improving over time.
+
+## UI Components
+
+The GUI is built using Tkinter and provides an easy-to-use interface for interacting with the neural network model. The key components include:
+
+### Activation and Cost Function Selection:
+
+- Drop-down menus allow you to select the activation and cost functions that best suit your needs.
+
+### Hyperparameter Inputs:
+
+- **Number of Epochs**: Input box to specify the number of training epochs.
+- **Learning Rate**: Input box to set the learning rate for model training.
+
+### Training Controls:
+
+- **Start Training Button**: Starts the training process, which will run in the background and update the UI in real-time.
+- **Reset Button**: Resets the training parameters and clears the progress graphs for a fresh training run.
+
+### Training Progress Display:
+
+- **Epoch/Cost Label**: Displays the current epoch and the cost value (loss) during training.
+- **Training Status**: A label to show the current status of the training (e.g., "Training in Progress").
+
+### Loss Graph:
+
+- The loss graph updates in real-time to show how the cost value changes over the epochs. This helps in visualizing the model's performance and diagnosing issues like underfitting or overfitting.
+
+### Neural Network Architecture Graph:
+
+- The architecture of the neural network is displayed visually. Layers are shown as nodes (circles), and the connections between them are represented as lines. This visualization helps users understand the structure of the network, including the number of neurons in each layer and how they are interconnected.
+
+## Neural Network Architecture
+
+The neural network consists of:
+
+- **Input Layer**: The input features of the dataset.
+- **Hidden Layer**: A layer with 64 neurons by default (can be adjusted).
+- **Output Layer**: The output, which predicts a binary or real-valued output.
+
+The architecture is visualized using a graph:
+
+- **Input Layer** and **Output Layer** are shown as circles.
+- Connections between layers are drawn as lines.
+
+### Neural Network Visualization
+
+The neural network architecture is displayed in a static plot that shows the structure of the network. Each layer's neurons are shown as nodes, with lines representing the connections between them.
+
+The neural network architecture provides insight into how the network is structured, helping you visualize:
+
+- The input and output layers.
+- The number of neurons in the hidden layers.
+- The connections (weights) between each layer.
+
+## Training Loss Graph
+
+The **Training Loss Graph** provides a dynamic view of how the loss function evolves during training:
+
+- The **x-axis** represents the number of epochs.
+- The **y-axis** represents the cost (loss) value at each epoch.
+- As the training progresses, the graph typically shows a decrease in the cost, indicating that the model is learning and improving its performance.
+
+### Interpreting the Loss Graph
+
+- **Decreasing Loss**: A downward slope in the graph indicates that the model is improving, i.e., the error is decreasing over time.
+- **Flat or Increasing Loss**: If the graph flattens or the loss increases, it could indicate that the model is not learning effectively, possibly due to a high learning rate or insufficient training time.
+
+## Example Dataset
+
+The neural network is initially configured to work with the **Banknote Authentication dataset** from UCI. This dataset is used for binary classification of banknote authenticity.
+
+The dataset includes:
+
+- **Features**: Variance, Skewness, Curtosis, Entropy.
+- **Target**: Authenticity of the banknote (1 = authentic, 0 = not authentic).
+
+You can modify the dataset or use your own data for training.
+
+## Future Enhancements
+
+- Support for different types of datasets.
+- Additional activation and cost functions.
+- Implementation of more complex neural network architectures (e.g., multiple hidden layers).
+- Early stopping to avoid overfitting.
+- Improved real-time loss graph visualization with interactive plots.
+
+
+## Acknowledgements
+
+- **UCI Machine Learning Repository** for providing the dataset.
+- **Tkinter** for the graphical interface.
+- **Matplotlib** for visualizing the loss graph and neural network architecture.
+
